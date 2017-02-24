70f0080def368a666e080265097071d4ef97b2f9
liuweilong96 <liuweilong0512@gmail.com>
2017-02-23 22:23:23 -0500

    Update and rename Adventurer.java to Alex.java

--- Alex.java
+++ Alex.java
@@ -2,7 +2,7 @@
  *
 An adventurer has:
  *A location (some room).
- *An inventory (the things being carried).  When the player executes the ¡°take thing¡± command, the item should be added to the inventory.
+ *An inventory (the things being carried).  When the player executes the Â¡Â°take thingÂ¡Â± command, the item should be added to the inventory.
 An adventurer can:
  *Move from room to room.
  *Carry a number of objects.
@@ -11,7 +11,7 @@ An adventurer can:
 * @version 1.0
 * Group Assignment 1 
 */
-public class Adventurer {
+public class Alex {
 	private boolean smallKey = false; // all the stuffs that are used for chapter 1
 	private boolean box = false;
 	private boolean WeddingPhoto = false;
@@ -19,7 +19,7 @@ public class Adventurer {
 	private boolean mountainPhoto = false;
 	private boolean roomkey = false;
 
-	public Adventurer(){
+	public Alex(){
 	}
 		public void akey(boolean key){ // this is the boolean while if the case is yes, story can keep going.
 			this.smallKey =!this.smallKey ;		
